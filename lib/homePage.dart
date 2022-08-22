6b1c21b717cb93a01633585bad8987aa990e23ae
Kadir Oner <kadironer46500@gmail.com>
2022-08-22 11:29:21 +0300

    Bottom navigation bar updated

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -4,7 +4,6 @@ import 'package:nft_market/bottomNavigationBarPage/profile%20Page.dart';
 import 'package:nft_market/bottomNavigationBarPage/search.dart';
 import 'package:nft_market/bottomNavigationBarPage/statisticPage.dart';
 import 'bottomNavigationBarPage/homeScreen.dart';
-import 'package:floating_bottom_bar/animated_bottom_navigation_bar.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -23,58 +22,125 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: bottomNavigationBarPageList[selectedIndex],
-
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        bottomBarItems: [
-          BottomBarItemsModel(
-            icon: Icon(Icons.home, color: Color(0xFF9a9fa3),),
-            title: "Home",
-            iconSelected: Icon(Icons.home, color: Color(0xFF4f7ae2),),
-            dotColor: Color(0xFF4f7ae2),
-            onTap:(){
-            }
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF4983ff),
+        child: Icon(Icons.add),
+        onPressed:(){
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        elevation: 10.0,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                    onPressed:(){
+                      setState((){
+                        selectedIndex=0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(selectedIndex==0 ? CupertinoIcons.house_alt_fill : CupertinoIcons.house_alt,
+                        color: selectedIndex==0 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                        ),
+                        Text("Home",
+                          style: TextStyle(
+                            color: selectedIndex==0 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                            fontWeight: FontWeight.bold
+                        ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    onPressed:(){
+                      setState((){
+                        selectedIndex=1;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(CupertinoIcons.search,
+                          color: selectedIndex==1 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                        ),
+                        Text("Search",
+                          style: TextStyle(
+                              color: selectedIndex==1 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                    onPressed:(){
+                      setState((){
+                        selectedIndex=2;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(CupertinoIcons.waveform_path_ecg,
+                          color: selectedIndex==2 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                        ),
+                        Text("Statistic",
+                          style: TextStyle(
+                              color: selectedIndex==2 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    onPressed:(){
+                      setState((){
+                        selectedIndex=3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(selectedIndex==3 ?  CupertinoIcons.person_fill : CupertinoIcons.person,
+                          color: selectedIndex==3 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                        ),
+                        Text("Profile",
+                          style: TextStyle(
+                              color: selectedIndex==3 ? Color(0xFF4983ff) : Color(0xFF9a9fa3),
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          BottomBarItemsModel(
-              icon: Icon(Icons.search, color: Color(0xFF9a9fa3),),
-              title: "Search",
-              iconSelected: Icon(Icons.search, color: Color(0xFF4f7ae2),),
-              dotColor: Color(0xFF4f7ae2),
-              onTap:(){
-              }
-          ),
-          BottomBarItemsModel(
-              icon: Icon(Icons.line_axis_outlined, color: Color(0xFF9a9fa3),),
-              title: "Statistic",
-              iconSelected: Icon(Icons.line_axis_outlined, color: Color(0xFF4f7ae2),),
-              dotColor: Color(0xFF4f7ae2),
-              onTap:(){
-              }
-          ),
-          BottomBarItemsModel(
-              icon: Icon(Icons.person, color: Color(0xFF9a9fa3),),
-              title: "Profile",
-              iconSelected: Icon(Icons.person, color: Color(0xFF4f7ae2),),
-              dotColor: Color(0xFF4f7ae2),
-              onTap:(){
-              }
-          ),
-        ],
-        bottomBarCenterModel: BottomBarCenterModel(
-          centerBackgroundColor: Color(0xFF4f7ae2),
-          centerIcon: FloatingCenterButton(
-            child: Icon(Icons.add),
-          ),
-            centerIconChild: [
-          FloatingCenterButtonChild(
-            child: const Icon(
-              Icons.home,
-              color: AppColors.white,
-            ),
-            onTap: () {},
-          ),
-        ]
         ),
       )
     );
   }
 }
+
+
+
+
